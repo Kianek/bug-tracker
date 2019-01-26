09737f0ec157e5c18b966ec47ea1a9c1a5e27a65
Keanu Allridge <kallridge92@gmail.com>
2019-01-26 14:05:31 -0800

    Initial commit

---
+++ server/bugs/BugController.js
@@ -0,0 +1,10 @@
+const Bug = require('./Bug');
+
+module.exports = {
+  bugFactory: ({ title, description }) => {},
+  addBug: () => {},
+  editBug: () => {},
+  deleteBug: () => {},
+  findBug: () => {},
+  findAllBugs: () => {},
+};
