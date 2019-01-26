1342b6964932b112e22fa4d42e0ceb6368e9a5be
Keanu Allridge <kallridge92@gmail.com>
2019-01-26 14:29:41 -0800

    Add body of bugFactory method

--- server/bugs/BugController.js
+++ server/bugs/BugController.js
@@ -1,7 +1,14 @@
 const Bug = require('./Bug');
+const { isEmpty } = require('validator');
 
 module.exports = {
-  bugFactory: ({ title, description }) => {},
+  bugFactory: (title, description) => {
+    if (isEmpty(title) || isEmpty(description)) {
+      return null;
+    }
+
+    return new Bug({ title, description });
+  },
   addBug: () => {},
   editBug: () => {},
   deleteBug: () => {},
