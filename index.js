47cae5ca787a9883eb6cef800d6b82f371a2a358
Keanu Allridge <kallridge92@gmail.com>
2019-01-26 12:30:29 -0800

    Add mongoose config

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const app = require('./server/app');
+const mongoose = require('mongoose');
 const { port } = require('./config/keys');
+
+const { mongoURI, mongoOpts } = require('./config/keys-dev');
+mongoose
+  .connect(
+    mongoURI,
+    mongoOpts
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
