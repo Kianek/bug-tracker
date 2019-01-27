1f711927340b43e5c7da5495cbbad9c53e88efb7
Keanu Allridge <kallridge92@gmail.com>
2019-01-26 20:54:38 -0800

    Import BugController and use 'add' route

--- routes/bugs.js
+++ routes/bugs.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const bugController = require('../server/bugs/BugController');
 
 router.get('/test', (req, res) => {
   res.send({ msg: 'Test successful' });
 });
+
+router.post('/add', bugController.addBug);
 
 module.exports = router;
