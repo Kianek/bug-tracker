32de0c594af5e6b285cd215065bb36e225a81d64
Keanu Allridge <kallridge92@gmail.com>
2019-01-26 20:51:11 -0800

    Refactor BugController; add 'addBug' functionality

--- server/bugs/BugController.js
+++ server/bugs/BugController.js
@@ -1,17 +1,36 @@
 const Bug = require('./Bug');
 const isEmpty = require('../../util/is-empty');
 
-module.exports = {
-  bugFactory: (title, description) => {
+function BugController() {
+  this.bugFactory = (title, description) => {
     if (typeof title !== 'string' || typeof description !== 'string') {
       return null;
     }
 
     return new Bug({ title, description });
-  },
-  addBug: () => {},
-  editBug: () => {},
-  deleteBug: () => {},
-  findBug: () => {},
-  findAllBugs: () => {},
-};
+  };
+
+  this.addBug = async (req, res) => {
+    const { title, description } = req.body;
+    const newBug = this.bugFactory(title, description);
+
+    if (!newBug) {
+      return res.status(400).json({ msg: 'Title and/or description invalid ' });
+    }
+
+    try {
+      const result = await newBug.save();
+      console.log(result);
+      return res.status(201).json(result);
+    } catch (err) {
+      return res.status(400).json({ error: 'Unable to add bug' });
+    }
+  };
+
+  this.editBug = async () => {};
+  this.deleteBug = async () => {};
+  this.findBug = async () => {};
+  this.findAllBugs = async () => {};
+}
+
+module.exports = new BugController();
