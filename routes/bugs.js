c4b9a19d8fa9a709412760e1a6f9203cca139650
Keanu Allridge <kallridge92@gmail.com>
2019-01-28 20:03:19 -0800

    Add findBug and findAllBugs routes

--- routes/bugs.js
+++ routes/bugs.js
@@ -5,6 +5,10 @@ router.get('/test', (req, res) => {
   res.send({ msg: 'Test successful' });
 });
 
+router.get('/', bugController.findAllBugs);
+
+router.get('/:id', bugController.findBug);
+
 router.post('/add', bugController.addBug);
 
 router.put('/:id/update', bugController.updateBug);
